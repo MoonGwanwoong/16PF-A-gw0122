be9462c74716832400a65f411ee46054070e69f8
CAD Client <CAD Client>
2016-04-13 20:32:32 +0900

    0413 CPF

---
+++ ex14/ex14.py
@@ -0,0 +1,24 @@
+#-*-coding:cp949
+from sys import argv
+
+script, user_name, a1, second1 = argv
+prompt = '>>>'
+
+print ("Hi, %s, I'm the %s script." % (user_name, script))
+print "I'd like to ask you a few questions."
+print "Do you like me %s?" % user_name
+likes = raw_input(prompt)
+
+print "Where do you live %s?" % user_name
+lives = raw_input(prompt)
+
+print "What kind of computer do you have?"
+computer = raw_input(prompt)
+
+print """
+Alright, so you said %s about liking me.
+You live in %r Not sure where that is.
+And you have a %s computer. NIce.
+""" % (likes, lives, computer)
+
+print "And you wrote in script parameter like this - %s, %s"% (a1, second1)
