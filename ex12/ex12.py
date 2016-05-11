dba446b06714578e02b45226bdc6a34cab1f72b1
CAD Client <CAD Client>
2016-05-11 19:24:06 +0900

    0511 commit2

--- ex12/ex12.py
+++
@@ -1,7 +0,0 @@
-age = raw_input("How old are you?")
-height = raw_input("How tall are you?")
-weight = raw_input("Howm much do you weigh?")
-abc = raw_input(10)
-
-print"So, you're %r old, %r tall and %r heavy.,%r10" % (
-    age, height, weight,abc)
