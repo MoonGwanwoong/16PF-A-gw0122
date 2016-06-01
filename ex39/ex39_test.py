57488bcd43ddde01962e94f09669648aa9fee815
CAD Client <CAD Client>
2016-06-01 19:26:27 +0900

    0601 commit1

---
+++ ex39/ex39_test.py
@@ -0,0 +1,8 @@
+states = {
+    'OS' : 'Operating System' ,
+    'CAD' : 'Computer Aided Design' ,
+    'CAM' : 'Computer Aided Manufacturing',
+    'CAE' : 'COmputer Aided Engineering'
+            }
+for c,a in states.items() :
+    print "%s : %s" % (c,a)
